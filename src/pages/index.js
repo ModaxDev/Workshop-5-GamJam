c1615d028205c38a1616a63803203551b33d3013
estelle-cs <casu.estelle@yahoo.com>
2023-04-12 17:05:59 +0200

    change index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import PageTuto from "./PageTuto.jsx";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import HomePage from "./HomePage.jsx";
+import UserNamePage from "./UserNamePage";
 
 export default function Home() {
   return (
     <>
-        <PageTuto />
-        <HomePage />
+      <UserNamePage />
     </>
   );
 }
