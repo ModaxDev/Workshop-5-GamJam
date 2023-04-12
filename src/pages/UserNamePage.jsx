428f703e1b13429ba9ce86360ea54a4409457983
estelle-cs <casu.estelle@yahoo.com>
2023-04-12 17:05:37 +0200

    add design to add pseudo page

--- src/pages/UserNamePage.jsx
+++ src/pages/UserNamePage.jsx
@@ -1,22 +1,51 @@
-import UserNameInput from '../components/UserNameInput.jsx'
-import React, { useState, useEffect } from 'react';
+import UserNameInput from "../components/UserNameInput.jsx";
+import React, { useState, useEffect } from "react";
 
-export const UserNamePage = ({setIsPseudo}) => {
+export const UserNamePage = ({ setIsPseudo }) => {
   const [Username, setUsername] = useState("Joueur 1");
 
   const onSubmit = async (data) => {
     setUsername(data?.username);
-    const res = await fetch("/api/InsertPseudo", { method: "POST", body: JSON.stringify(data?.username) });
+    const res = await fetch("/api/InsertPseudo", {
+      method: "POST",
+      body: JSON.stringify(data?.username),
+    });
     const resultat = await res.json();
-    setIsPseudo(true)
+    setIsPseudo(true);
   };
-  
-return(
+
+  return (
     <>
-        <h2 className="input-title">Votre pseudo : {Username}</h2>
-        <UserNameInput onSubmit = {(data) => onSubmit(data)}/>
+      <body
+        style={{
+          "background-color": "#0075E1",
+          height: "100%",
+          color: "white",
+          textAlign: "center",
+          padding: "60px",
+        }}
+      >
+        <h1
+          style={{
+            "font-family": "Luckiest Guy, cursive",
+            "font-size": "70px",
+          }}
+        >
+          React Couleur
+        </h1>
+        <h2
+          className="input-title"
+          style={{
+            "font-family": "Concert One, cursive",
+            "margin-bottom": "50px",
+          }}
+        >
+          Quel est ton pseudo ? {Username}
+        </h2>
+        <UserNameInput onSubmit={(data) => onSubmit(data)} />
+      </body>
     </>
-  )
-}
+  );
+};
 
 export default UserNamePage;
